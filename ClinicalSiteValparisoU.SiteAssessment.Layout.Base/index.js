f7f6909cf47e432f2a1f82a16208345d404fd884
harsh-sleeksky <harsh@sleeksky.com>
2024-07-08 17:19:42 +0530

    updates

---
+++ ClinicalSiteValparisoU.SiteAssessment.Layout.Base/index.js
@@ -0,0 +1,22 @@
+function updateResponse(response) {
+  let sectionIndex = response.sections.findIndex(
+    (section) =>
+      section.sectionId == "Section.SiteAssessment.ClinicalSiteInformation"
+  );
+
+  if (sectionIndex !== -1) {
+    const updatedvalues = {};
+
+    for (const [key, value] of Object.entries(
+      response.sections[sectionIndex].values
+    )) {
+      if (key == "B13" && ["Yes", "No"].includes(value)) {
+        updatedvalues["B131"] = value;
+      } else updatedvalues[key] = value;
+    }
+
+    response.sections[sectionIndex].values = updatedvalues;
+  }
+
+  return response;
+}
