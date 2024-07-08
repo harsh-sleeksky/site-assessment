1dea7af5e4a84c1808c678c4635c859c81baad16
harsh-sleeksky <harsh@sleeksky.com>
2024-07-08 17:29:33 +0530

    updates

--- ClinicalSiteValparisoU.SiteAssessment.Layout.Base/index.js
+++ ClinicalSiteValparisoU.SiteAssessment.Layout.Base/index.js
@@ -1,6 +1,6 @@
 function updateResponse(response) {
   let sectionIndex = response.sections.findIndex(
-    (section) => section.sectionId == "Section.SiteAssessment.MidtermMeeting"
+    (section) => section.sectionId == "Section.SiteAssessment.SiteInformation"
   );
 
   if (sectionIndex !== -1) {
@@ -9,9 +9,29 @@ function updateResponse(response) {
     for (const [key, value] of Object.entries(
       response.sections[sectionIndex].values
     )) {
-      if (key == "DP1") {
-        const parentValue = response.sections[sectionIndex].values["B17"];
-        if (parentValue == "Maybe") updatedvalues["DP1-Maybe"] = value;
+      if (key == "Adequatesupervisionchild:") {
+        const parentValue =
+          response.sections[sectionIndex].values["Adequatesupervision"];
+        if (parentValue == "No")
+          updatedvalues["Adequatesupervisionchild1:"] = value;
+      } else updatedvalues[key] = value;
+    }
+
+    response.sections[sectionIndex].values = updatedvalues;
+  }
+
+  sectionIndex = response.sections.findIndex(
+    (section) => section.sectionId == "Section.SiteAssessment.OTHER"
+  );
+
+  if (sectionIndex !== -1) {
+    const updatedvalues = {};
+
+    for (const [key, value] of Object.entries(
+      response.sections[sectionIndex].values
+    )) {
+      if (key == "Attendsorganized") {
+        updatedvalues["Attendsorganized1"] = value;
       } else updatedvalues[key] = value;
     }
 
