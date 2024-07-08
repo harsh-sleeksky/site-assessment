48ca64991ea21453707ebb13af77a7168e568a07
harsh-sleeksky <harsh@sleeksky.com>
2024-07-08 12:37:48 +0530

    updates

---
+++ GWUSMHS.SiteAssessment.Layout.Base/index.js
@@ -0,0 +1,23 @@
+function updateResponse(response) {
+  const sectionIndex = response.sections.findIndex(
+    (section) =>
+      section.sectionId == "Section.SiteAssessment.DiscussionInstructor"
+  );
+
+  if (sectionIndex !== -1) {
+    const updatedvalues = {};
+
+    for (const [key, value] of Object.entries(
+      response.sections[sectionIndex].values
+    )) {
+      if (key == "safety") {
+        if (["Yes", "No"].includes(value)) updatedvalues["safety1"] = value;
+        else updatedvalues["safety2"] = value;
+      } else updatedvalues[key] = value;
+    }
+
+    response.sections[sectionIndex].values = updatedvalues;
+  }
+
+  return response;
+}
