d4cab224c52b31a22f79637398f8a22089864df3
harsh-sleeksky <harsh@sleeksky.com>
2024-07-08 14:38:06 +0530

    updates

---
+++ RUSHPA.SiteAssessment.Layout.Base/index.js
@@ -0,0 +1,29 @@
+function updateResponse(response) {
+  let sectionIndex = response.sections.findIndex(
+    (section) =>
+      section.sectionId == "Section.SiteAssessment.RUSHPA_ClinicalSite"
+  );
+
+  if (sectionIndex !== -1) {
+    const updatedvalues = {};
+
+    for (const [key, value] of Object.entries(
+      response.sections[sectionIndex].values
+    )) {
+      if (
+        key == "LO19" &&
+        [
+          "Excellent—the rotation site consistently exceeds program expectations. ",
+          "Good—the rotation site consistently meets program expectations",
+          "Poor—the rotation site consistently does not meet program expectations.",
+        ].includes(value)
+      ) {
+        updatedvalues["LO191"] = value;
+      } else updatedvalues[key] = value;
+    }
+
+    response.sections[sectionIndex].values = updatedvalues;
+  }
+
+  return response;
+}
