a861aed38cc1c06a2a7d43e7561da8846d0a6d6a
harsh-sleeksky <harsh@sleeksky.com>
2024-07-11 10:16:30 +0530

    updates

---
+++ EVMSPA.SiteAssessment.Layout.Base/index.js
@@ -0,0 +1,43 @@
+function correctResponse(response) {
+  let sectionIndex = response.sections.findIndex(
+    (section) =>
+      section.sectionId == "Section.SiteAssessment.Additionallearning"
+  );
+
+  if (sectionIndex !== -1) {
+    const updatedvalues = {};
+
+    for (const [key, value] of Object.entries(
+      response.sections[sectionIndex].values
+    )) {
+      if (key == "Ruralpopulation") {
+        updatedvalues[`${key}1`] = value;
+      } else updatedvalues[key] = value;
+    }
+
+    response.sections[sectionIndex].values = updatedvalues;
+  }
+
+  sectionIndex = response.sections.findIndex(
+    (section) =>
+      section.sectionId == "Section.SiteAssessment.StudentExpectations"
+  );
+
+  if (sectionIndex !== -1) {
+    const updatedvalues = {};
+
+    for (const [key, value] of Object.entries(
+      response.sections[sectionIndex].values
+    )) {
+      if (key == "Ruralpopulation") {
+        updatedvalues[`${key}2`] = value;
+      } else updatedvalues[key] = value;
+    }
+
+    response.sections[sectionIndex].values = updatedvalues;
+  }
+
+  return response;
+}
+
+module.exports.method = correctResponse;
